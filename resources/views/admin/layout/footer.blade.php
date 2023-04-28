22c6c8268715aae3ea059e603a86d8f964c4c898
avrians <avriansyahb10@gmail.com>
2023-04-28 16:46:04 +0700

    add konten footer admin

---
+++ resources/views/admin/layout/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="sticky-footer bg-white">
+    <div class="container my-auto">
+        <div class="copyright text-center my-auto">
+            <span>Copyright &copy; SMK Harapan Bangsa 2023</span>
+        </div>
+    </div>
+</footer>
