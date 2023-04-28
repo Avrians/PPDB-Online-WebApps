b11fe925d1651671ccb642b1bb764fb766772ff0
avrians <avriansyahb10@gmail.com>
2023-04-28 16:51:02 +0700

    add new file layout footer

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,18 @@
+<footer id="footer" style="margin-top: 100px;">
+    <div class="container d-md-flex py-4">
+
+      <div class="mr-md-auto text-center text-md-left">
+        <div class="copyright">
+          &copy; Copyright 2023 <strong><span>SMK Harapan Bangsa</span></strong>. All Rights Reserved <br>
+          Powered By. Avriansyah Bahtiar
+        </div>
+      </div>
+      <div class="social-links text-center text-md-right pt-3 pt-md-0">
+        <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+        <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+        <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+        <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
+        <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+      </div>
+    </div>
+  </footer>
