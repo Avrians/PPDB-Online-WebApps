f37271d81c95ee70e1616e0cb9c4d6bcd5069a04
avrians <avriansyahb10@gmail.com>
2023-04-28 16:45:10 +0700

    add new file admin index dan footer

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -6,5 +6,11 @@ use Illuminate\Http\Request;
 
 class RegisterController extends Controller
 {
-    //
+    public function index() {
+
+    }
+
+    public function store(Request $request) {
+
+    }
 }
