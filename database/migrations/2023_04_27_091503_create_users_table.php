25cc9eb7558c5dcbab68fb17ec7cf2c33ae739a6
avrians <avriansyahb10@gmail.com>
2023-05-02 19:06:40 +0700

    update

--- database/migrations/2023_04_27_091503_create_users_table.php
+++ database/migrations/2023_04_27_091503_create_users_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
             $table->timestamps();
         });
     }
