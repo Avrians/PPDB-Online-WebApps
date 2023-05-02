dfbfa8ce326819463dc8e51f79414293a472c884
avrians <avriansyahb10@gmail.com>
2023-05-02 19:04:36 +0700

    update isi table

--- database/migrations/2023_05_01_081023_create_pendaftarans_table.php
+++ database/migrations/2023_05_01_081023_create_pendaftarans_table.php
@@ -20,6 +20,11 @@ return new class extends Migration
             $table->date('tanggal_lahir');
             $table->string('asal_sekolah');
             $table->enum('jenik_kelamin', ['L', 'P']);
+            $table->string('email')->unique()->nullable();
+            $table->string('nilai_indo');
+            $table->string('nilai_mtk');
+            $table->string('nilai_inggris');
+            $table->string('jarak');
             $table->text('alamat');
             $table->timestamps();
         });
