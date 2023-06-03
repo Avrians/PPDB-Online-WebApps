5056396e924f775329f8e47e6db9bf91fd5ba8b6
avrians <avriansyahb10@gmail.com>
2023-06-03 16:18:06 +0700

    add halaman login

---
+++ database/migrations/2023_06_03_075421_create_scores_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('scores', function (Blueprint $table) {
+            $table->id();
+            $table->string('siswa_calons_id');
+            $table->string('categories_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('scores');
+    }
+};
